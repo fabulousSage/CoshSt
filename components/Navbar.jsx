407ed41363045b91d542efdd2e286ec2de2600e9
fabulouzsage <fabuloussage@yahoo.com>
2023-05-12 20:53:54 +0100

    Antiquity

---
+++ components/Navbar.jsx
@@ -0,0 +1,37 @@
+import React from "react";
+import Link from "next/link";
+import { AiOutlineShopping } from "react-icons/ai";
+
+import Cart from "./Cart";
+import { useStateContext } from "../context/StateContext";
+
+const Navabar = () => {
+  const { showCart, setShowCart, totalQuantities, showIconCart } =
+    useStateContext();
+
+  return (
+    <div className="navbar-wrapper">
+      <div className="navbar-container">
+        <p className="logo">
+          <Link href="/">LogoCandles</Link>
+        </p>
+
+        {showIconCart && (
+          <button
+            type="button"
+            className="cart-icon"
+            onClick={() => setShowCart(true)}
+          >
+            <AiOutlineShopping />
+
+            <span className="cart-item-qty">{totalQuantities}</span>
+          </button>
+        )}
+
+        {showCart && <Cart />}
+      </div>
+    </div>
+  );
+};
+
+export default Navabar;
